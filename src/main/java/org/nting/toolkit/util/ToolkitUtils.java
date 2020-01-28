8b793b61f2bb579a4f4b74ba420ab36554aac927
Zsolti <zsolt.borcsok@gmail.com>
2020-01-28 18:09:47 +0100

    Extend ToolkitUtils with new utility methods

--- src/main/java/org/nting/toolkit/util/ToolkitUtils.java
+++ src/main/java/org/nting/toolkit/util/ToolkitUtils.java
@@ -4,13 +4,19 @@ import static org.nting.toolkit.ToolkitServices.toolkitManager;
 import static org.nting.toolkit.util.GwtCompatibleUtils.isAssignableFrom;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.nting.data.Property;
 import org.nting.toolkit.Component;
+import org.nting.toolkit.component.AbstractTextComponent;
+import org.nting.toolkit.component.Popup;
+import org.nting.toolkit.component.ScrollComponent;
 
 import com.google.common.collect.Lists;
 
 import pythagoras.f.Dimension;
 import pythagoras.f.Point;
+import pythagoras.f.Rectangle;
 
 public class ToolkitUtils {
 
@@ -112,7 +118,52 @@ public class ToolkitUtils {
         return false;
     }
 
-    @SuppressWarnings("unchecked")
+    public static Property<Component> getFocusOwner() {
+        return toolkitManager().keyDispatcher().getFocusOwner();
+    }
+
+    public static Property<AbstractTextComponent> getForcedFocusOwner() {
+        return toolkitManager().keyDispatcher().getForcedFocusOwner();
+    }
+
+    public static Popup getPopupAncestor(Component component) {
+        for (Component parent = component.getParent(); parent != null; parent = parent.getParent()) {
+            if (parent instanceof Popup) {
+                return (Popup) parent;
+            }
+        }
+        return null;
+    }
+
+    public static ScrollComponent getScrollComponentAncestor(Component component) {
+        for (Component parent = component.getParent(); parent != null; parent = parent.getParent()) {
+            if (parent instanceof ScrollComponent) {
+                return (ScrollComponent) parent;
+            }
+        }
+        return null;
+    }
+
+    // TODO
+    // public static LayeredPane getLayeredPaneAncestor(Component component) {
+    // for (Component parent = component.getParent(); parent != null; parent = parent.getParent()) {
+    // if (parent instanceof LayeredPane) {
+    // return (LayeredPane) parent;
+    // }
+    // }
+    // return null;
+    // }
+    //
+    // public static List<Stone> getAllStones(Stone root) {
+    // List<Stone> stones = Lists.newLinkedList();
+    // stones.add(root);
+    // for (int i = 0; i < stones.size(); i++) {
+    // stones.addAll(i + 1, stones.get(i).children());
+    // }
+    //
+    // return stones;
+    // }
+
     public static <C extends Component> C getComponentById(Component parent, String... ids) {
         if (ids.length == 0) {
             return null;
@@ -134,6 +185,34 @@ public class ToolkitUtils {
         }
 
         return null;
+    }
+
+    public static void scrollComponentToVisible(Component component) {
+        ScrollComponent scrollComponent = getScrollComponentAncestor(component);
+        if (scrollComponent != null && scrollComponent.getView() != component) {
+            Point position = getPosition(scrollComponent, component);
+            Point viewPosition = scrollComponent.getViewPosition();
+            position.addLocal(viewPosition.x, viewPosition.y);
+            Rectangle componentRectangle = new Rectangle(position, component.getSize());
+            scrollComponent.scrollRectToVisible(componentRectangle);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Component> Optional<T> getFirstChildWithType(Component component, Class<T> componentType) {
+        List<Component> components = Lists.newLinkedList();
+        components.add(component);
+        for (int i = 0; i < components.size(); i++) {
+            Component currentComponent = components.get(i);
+            if (!currentComponent.isVisible()) {
+                components.remove(i--);
+            } else if (isAssignableFrom(componentType, currentComponent.getClass())) {
+                return (Optional<T>) Optional.of(currentComponent);
+            } else {
+                components.addAll(i + 1, currentComponent.getComponents());
+            }
+        }
+        return Optional.empty();
     }
 
     public static float minOfDimensions(Component component) {
