f47e722ac597ec4f380f3ad113b1eeaee0472e70
Zsolti <zsolt.borcsok@gmail.com>
2020-01-30 11:15:16 +0100

    Panel just paints background color or image and could have a default button

--- src/main/java/org/nting/toolkit/component/Panel.java
+++ src/main/java/org/nting/toolkit/component/Panel.java
@@ -1,5 +1,70 @@
 package org.nting.toolkit.component;
 
+import org.nting.data.Property;
+import org.nting.toolkit.event.ActionEvent;
+import org.nting.toolkit.event.KeyEvent;
+import org.nting.toolkit.event.KeyListener;
+import org.nting.toolkit.layout.AbsoluteLayout;
+import org.nting.toolkit.layout.LayoutManager;
+import org.nting.toolkit.ui.ComponentUI;
+
+import playn.core.Canvas;
+import playn.core.Image;
+import playn.core.Key;
+
 public class Panel extends AbstractComponent {
-    // TBD
+
+    /** If you define backgroundImage, then backgroundColor is ignored */
+    public final Property<Image> backgroundImage = createProperty("backgroundImage", null);
+    public final Property<Integer> backgroundColor = createProperty("backgroundColor", 0x00ffffff);
+
+    private Button defaultButton;
+
+    public Panel(String... reLayoutPropertyNames) {
+        this(new AbsoluteLayout(), reLayoutPropertyNames);
+    }
+
+    public Panel(LayoutManager layoutManager, String... reLayoutPropertyNames) {
+        super(reLayoutPropertyNames);
+        setLayoutManager(layoutManager);
+
+        addKeyListener(new KeyHandler());
+
+        setFocusable(false);
+    }
+
+    public Button getDefaultButton() {
+        return defaultButton;
+    }
+
+    public void setDefaultButton(Button defaultButton) {
+        this.defaultButton = defaultButton;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public void setComponentUI(ComponentUI componentUI) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void doPaintComponent(Canvas canvas) {
+        Image backgroundImage = this.backgroundImage.getValue();
+        if (backgroundImage != null) {
+            canvas.drawImage(backgroundImage, 0, 0, width.getValue(), height.getValue());
+        } else {
+            canvas.setFillColor(backgroundColor.getValue());
+            canvas.fillRect(0, 0, width.getValue(), height.getValue());
+        }
+    }
+
+    private class KeyHandler implements KeyListener {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (defaultButton != null && e.isKeyCode(Key.ENTER) && defaultButton.enabled.getValue()) {
+                defaultButton.actions().fireActionPerformed(new ActionEvent(defaultButton, e.getTime(), e));
+            }
+        }
+    }
 }
