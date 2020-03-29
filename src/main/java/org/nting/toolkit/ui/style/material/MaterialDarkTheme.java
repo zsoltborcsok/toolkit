5bc1addd8c560cf2e07e4b52741c3412a9a4e305
Zsolti <zsolt.borcsok@gmail.com>
2020-03-29 07:16:52 +0200

    Use material colors in the material themes

--- src/main/java/org/nting/toolkit/ui/style/material/MaterialDarkTheme.java
+++ src/main/java/org/nting/toolkit/ui/style/material/MaterialDarkTheme.java
@@ -1,16 +1,18 @@
 package org.nting.toolkit.ui.style.material;
 
-import static org.nting.toolkit.ui.Colors.BLACK;
 import static org.nting.toolkit.ui.Colors.WHITE;
+import static org.nting.toolkit.ui.style.material.MaterialColorPalette.grey_400;
+import static org.nting.toolkit.ui.style.material.MaterialColorPalette.grey_700;
+import static org.nting.toolkit.ui.style.material.MaterialColorPalette.grey_800;
 
 public class MaterialDarkTheme {
 
-    public static final int DARK_THEME_BACKGROUND_COLOR = WHITE;
-    public static final int DARK_THEME_BASE_COLOR = BLACK;
-    public static final int DARK_THEME_TEXT_COLOR = BLACK;
-    public static final int DARK_THEME_SECONDARY_COLOR = 0xFFbcbcbc; /* for secondary text and icons */
-    public static final int DARK_THEME_DISABLED_COLOR = 0xFF646464; /* disabled/hint text */
-    public static final int DARK_THEME_DIVIDER_COLOR = 0xFF3c3c3c;
+    public static final int DARK_THEME_BACKGROUND_COLOR = 0xFF121212;
+    public static final int DARK_THEME_BASE_COLOR = WHITE;
+    public static final int DARK_THEME_TEXT_COLOR = WHITE;
+    public static final int DARK_THEME_SECONDARY_COLOR = grey_400; /* for secondary text and icons */
+    public static final int DARK_THEME_DISABLED_COLOR = grey_700; /* disabled/hint text */
+    public static final int DARK_THEME_DIVIDER_COLOR = grey_800;
 
     public static final int DARK_DIVIDER_OPACITY = 31; // 12%
     public static final int DARK_DISABLED_OPACITY = 97; // 38% or hint text or icon
--- src/main/java/org/nting/toolkit/ui/style/material/MaterialLightTheme.java
+++ src/main/java/org/nting/toolkit/ui/style/material/MaterialLightTheme.java
@@ -2,6 +2,9 @@ package org.nting.toolkit.ui.style.material;
 
 import static org.nting.toolkit.ui.Colors.BLACK;
 import static org.nting.toolkit.ui.Colors.WHITE;
+import static org.nting.toolkit.ui.style.material.MaterialColorPalette.grey_300;
+import static org.nting.toolkit.ui.style.material.MaterialColorPalette.grey_500;
+import static org.nting.toolkit.ui.style.material.MaterialColorPalette.grey_600;
 import static org.nting.toolkit.ui.style.material.MaterialColorPalette.grey_900;
 
 public class MaterialLightTheme {
@@ -9,9 +12,9 @@ public class MaterialLightTheme {
     public static final int LIGHT_THEME_BACKGROUND_COLOR = WHITE;
     public static final int LIGHT_THEME_BASE_COLOR = BLACK;
     public static final int LIGHT_THEME_TEXT_COLOR = grey_900;
-    public static final int LIGHT_THEME_SECONDARY_COLOR = 0xFF737373; /* for secondary text and icons */
-    public static final int LIGHT_THEME_DISABLED_COLOR = 0xFF9b9b9b; /* disabled/hint text */
-    public static final int LIGHT_THEME_DIVIDER_COLOR = 0xFFdbdbdb;
+    public static final int LIGHT_THEME_SECONDARY_COLOR = grey_600; /* for secondary text and icons */
+    public static final int LIGHT_THEME_DISABLED_COLOR = grey_500; /* disabled/hint text */
+    public static final int LIGHT_THEME_DIVIDER_COLOR = grey_300;
 
     public static final int LIGHT_DIVIDER_OPACITY = 31; // 12%
     public static final int LIGHT_DISABLED_OPACITY = 77; // 30% or hint text or icon
