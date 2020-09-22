ab810bc1e219eefa9bd92da99aafe4e4e261cd0c
zsoltborcsok <zsolt.borcsok@gmail.com>
2020-09-22 10:22:17 +0200

    Refactor the ToolkitDeveloperTool into a separate type

---
+++ src/main/java/org/nting/toolkit/app/tools/ToolkitDeveloperTool.java
@@ -0,0 +1,74 @@
+package org.nting.toolkit.app.tools;
+
+import static org.nting.data.binding.Bindings.BindingStrategy.READ;
+import static org.nting.toolkit.ToolkitServices.developerTools;
+import static org.nting.toolkit.component.builder.ContainerBuilder.panelBuilder;
+import static org.nting.toolkit.layout.FormLayout.xy;
+
+import org.nting.data.Property;
+import org.nting.data.binding.Bindings;
+import org.nting.data.property.ObjectProperty;
+import org.nting.toolkit.ToolkitManager;
+import org.nting.toolkit.component.Panel;
+import org.nting.toolkit.component.builder.ContainerBuilder;
+
+import playn.core.Color;
+import playn.core.ImageLayer;
+import playn.core.PlayN;
+import pythagoras.f.Point;
+
+public class ToolkitDeveloperTool {
+
+    private Property<String> currentFps = new ObjectProperty<>("N/A");
+    private Property<String> mousePosition = new ObjectProperty<>("N/A");
+    private Property<Integer> color = new ObjectProperty<>(0);
+    private Property<String> argbColor = new ObjectProperty<>("N/A");
+    private Property<String> htmlColor = new ObjectProperty<>("N/A");
+    private Property<Boolean> isToolkitDeveloperToolActive = new ObjectProperty<>(false);
+
+    private final ToolkitManager toolkitManager;
+
+    public ToolkitDeveloperTool(ToolkitManager toolkitManager) {
+        this.toolkitManager = toolkitManager;
+        registerToolkitDeveloperTool();
+    }
+
+    public void updateToolkitDeveloperTool(float fps) {
+        if (!isToolkitDeveloperToolActive.getValue()) {
+            return;
+        }
+
+        currentFps.setValue(String.valueOf(fps));
+        Point position = toolkitManager.mouseDispatcher().getLastMousePosition(toolkitManager.root());
+        mousePosition.setValue("(" + position.x + ", " + position.y + ")");
+        int[][] imageData = ((ImageLayer) PlayN.graphics().rootLayer().get(0)).image().getImageData((int) position.x,
+                (int) position.y, 1, 1);
+        argbColor.setValue("ARGB(" + imageData[0][0] + ", " + imageData[0][1] + ", " + imageData[0][2] + ", "
+                + imageData[0][3] + ")");
+        color.setValue(Color.argb(imageData[0][0], imageData[0][1], imageData[0][2], imageData[0][3]));
+        htmlColor.setValue("HTML(#" + Integer.toHexString(color.getValue()) + ")");
+    }
+
+    private void registerToolkitDeveloperTool() {
+        ContainerBuilder<Panel, ?> panelBuilder = panelBuilder("right:max(pref;64dlu), 3dlu, 0px:grow", "");
+
+        panelBuilder.formLayout().addRow("4dlu").addRow("baseline:pref");
+        panelBuilder.addLabel(xy(0, panelBuilder.formLayout().lastRow())).text("Frames Per Second").pass()
+                .color(0xFF999999);
+        panelBuilder.addLabel(xy(2, panelBuilder.formLayout().lastRow())).text(currentFps);
+
+        panelBuilder.formLayout().addRow("4dlu").addRow("baseline:pref");
+        panelBuilder.addLabel(xy(0, panelBuilder.formLayout().lastRow())).text("Mouse position").pass()
+                .color(0xFF999999);
+        panelBuilder.addLabel(xy(2, panelBuilder.formLayout().lastRow())).text(mousePosition);
+
+        panelBuilder.formLayout().addRow("4dlu").addRow("baseline:pref").addRow("4dlu").addRow("pref");
+        panelBuilder.addLabel(xy(0, panelBuilder.formLayout().lastRow(2))).text("Color at mouse").pass()
+                .color(0xFF999999);
+        panelBuilder.addLabel(xy(2, panelBuilder.formLayout().lastRow(2))).text(argbColor);
+        panelBuilder.addLabel(xy(2, panelBuilder.formLayout().lastRow())).text(htmlColor);
+
+        Property<Boolean> isToolActive = developerTools().addTool("Toolkit", panelBuilder.build());
+        Bindings.bind(READ, isToolActive, isToolkitDeveloperToolActive);
+    }
+}
