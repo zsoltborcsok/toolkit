27e2fd97b76b628b918685cd461558f140ca7c5c
zsoltborcsok <zsolt.borcsok@gmail.com>
2020-09-23 16:37:13 +0200

    Support adjusting transparency of the DeveloperTools
    - Show this as a "Settings" tool of the DeveloperTools (a little bit
      ugly approach).
    - Minor style improvements, like paint separator on the left and
      smaller show/hide icons.

--- src/main/java/org/nting/toolkit/internal/DeveloperToolsImpl.java
+++ src/main/java/org/nting/toolkit/internal/DeveloperToolsImpl.java
@@ -1,20 +1,25 @@
 package org.nting.toolkit.internal;
 
+import static org.nting.data.binding.Bindings.BindingStrategy.READ;
 import static org.nting.toolkit.FontManager.FontSize.LARGE_FONT;
+import static org.nting.toolkit.FontManager.FontSize.MEDIUM_FONT;
 import static org.nting.toolkit.ToolkitServices.fontManager;
 import static org.nting.toolkit.ToolkitServices.unitConverter;
 import static org.nting.toolkit.component.builder.ContainerBuilder.panelBuilder;
 import static org.nting.toolkit.event.MouseListener.onClick;
 import static org.nting.toolkit.layout.FormLayout.xy;
 import static org.nting.toolkit.layout.FormLayout.xyw;
+import static org.nting.toolkit.util.ColorUtils.adjustAlpha;
 import static playn.core.Font.Style.BOLD;
 import static playn.core.Font.Style.ITALIC;
 import static playn.core.Platform.Type.HTML;
+import static pythagoras.i.MathUtil.clamp;
 
 import java.util.List;
 
 import org.nting.data.Property;
 import org.nting.data.Registration;
+import org.nting.data.binding.Bindings;
 import org.nting.data.condition.Conditions;
 import org.nting.toolkit.Component;
 import org.nting.toolkit.DeveloperTools;
@@ -28,20 +33,30 @@ import org.nting.toolkit.component.Panel;
 import org.nting.toolkit.component.ScrollPane;
 import org.nting.toolkit.component.Separator;
 import org.nting.toolkit.component.SimpleIconComponent;
+import org.nting.toolkit.component.StoneComponent;
 import org.nting.toolkit.component.TextAlignment;
 import org.nting.toolkit.component.builder.ContainerBuilder;
 import org.nting.toolkit.event.KeyEvent;
 import org.nting.toolkit.event.KeyListener;
 import org.nting.toolkit.layout.AbsoluteLayout;
 import org.nting.toolkit.layout.VerticalLayout;
+import org.nting.toolkit.ui.shape.RectangleShape;
+import org.nting.toolkit.ui.stone.Background;
 
+import playn.core.Canvas;
 import playn.core.Key;
 import playn.core.PlayN;
 import pythagoras.f.Dimension;
 
 public class DeveloperToolsImpl extends AbstractComponent implements DeveloperTools {
 
+    // No real styling, just fixed values
+    private static final int BACKGROUND_COLOR = 0xFFefebe9;
+    private static final int SEPARATOR_COLOR = 0xFFcccccc;
+    private static final int CAPTION_COLOR = 0xFF999999;
+
     private final Property<Boolean> isActivated = createProperty("isActivated", false);
+    private final Property<Float> alpha = createProperty("alpha", 1.0f);
 
     private final Panel content = new Panel(new VerticalLayout());
     private Registration keyListenerRegistration;
@@ -57,11 +72,15 @@ public class DeveloperToolsImpl extends AbstractComponent implements DeveloperTo
     // DeveloperTools is registered statically in the ToolkitServices together with ToolkitManager (used often),
     // but these services should be instantiated without referring each other.
     private void init() {
-        content.backgroundColor.setValue(0xFFefebe9); // Transparency works as well: 0x7F9e9e9e
+        Property<Integer> background = alpha
+                .transform(alpha -> adjustAlpha(clamp((int) (alpha * 255), 0, 255), BACKGROUND_COLOR));
+
+        Bindings.bind(READ, background, content.backgroundColor);
         List<Component> alreadyRegisteredTools = content.getComponents();
         content.removeAllComponents();
         content.addComponent(new Label().set("text", "Developer Tools")
                 .set("font", fontManager().getFont(LARGE_FONT, BOLD)).set("alignment", TextAlignment.CENTER));
+        addTool("Settings", createSettings(background));
         alreadyRegisteredTools.forEach(content::addComponent);
     }
 
@@ -109,7 +128,8 @@ public class DeveloperToolsImpl extends AbstractComponent implements DeveloperTo
         panelBuilder.addLabel(xy(1, panelBuilder.formLayout().lastRow())).text(name).pass().font(LARGE_FONT, ITALIC);
         panelBuilder.addComponent(new SimpleIconComponent(null), xy(2, panelBuilder.formLayout().lastRow()))
                 .<Boolean, Icon> bind("icon", tool.visible, visible -> visible ? FontIcon.EYE : FontIcon.EYE_SLASH)
-                .getComponent().addMouseListener(onClick(e -> tool.visible.adjustValue(b -> !b)));
+                .set("fontSize", MEDIUM_FONT).getComponent()
+                .addMouseListener(onClick(e -> tool.visible.adjustValue(b -> !b)));
         panelBuilder.formLayout().addRow("pref");
         panelBuilder.addComponent(new Separator(Orientation.HORIZONTAL),
                 xyw(1, panelBuilder.formLayout().lastRow(), 2));
@@ -118,5 +138,26 @@ public class DeveloperToolsImpl extends AbstractComponent implements DeveloperTo
         content.addComponent(panelBuilder.build());
 
         return Conditions.convert(isActivated).and(tool.visible);
+    }
+
+    @Override
+    public void doPaintForeground(Canvas canvas) {
+        super.doPaintForeground(canvas);
+
+        canvas.setFillColor(SEPARATOR_COLOR).fillRect(1, 0, 1, height.getValue());
+    }
+
+    private Panel createSettings(Property<Integer> background) {
+        ContainerBuilder<Panel, ?> panelBuilder = panelBuilder("right:max(pref;64dlu), 3dlu, 0px:grow",
+                "pref, 4dlu, pref");
+        panelBuilder.addComponent(new Separator(Orientation.HORIZONTAL),
+                xyw(0, panelBuilder.formLayout().lastRow(2), 3));
+        panelBuilder.addLabel(xy(0, panelBuilder.formLayout().lastRow())).text("Alpha").pass().color(CAPTION_COLOR);
+        panelBuilder.addComponent(new StoneComponent(null), xy(2, panelBuilder.formLayout().lastRow()))
+                .bind("stone", background,
+                        c -> new Background(new RectangleShape().strokeColor(SEPARATOR_COLOR).fillColor(c)))
+                .addMouseListener(onClick(e -> alpha.setValue(e.getX() / e.getSource().getSize().width)));
+        panelBuilder.formLayout().addRow("4dlu");
+        return panelBuilder.build();
     }
 }
