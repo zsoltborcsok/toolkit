19ad0abfb95f51ee02f51764e9690ca769a8e211
zsoltborcsok <zsolt.borcsok@gmail.com>
2020-09-27 18:40:51 +0200

    Support selecting the type of the shown popup in PopupTestPage

--- src/test/java/org/nting/toolkit/app/pages/PopupTestPage.java
+++ src/test/java/org/nting/toolkit/app/pages/PopupTestPage.java
@@ -5,9 +5,9 @@ import static org.nting.toolkit.component.builder.ContainerBuilder.panelBuilder;
 import static org.nting.toolkit.layout.FormLayout.xy;
 import static org.nting.toolkit.layout.FormLayout.xyw;
 import static playn.core.Font.Style.BOLD;
+import static playn.core.util.SimpleMessageFormat.format;
 
 import org.nting.data.Property;
-import org.nting.data.property.ObjectProperty;
 import org.nting.toolkit.Component;
 import org.nting.toolkit.app.IPageFactory;
 import org.nting.toolkit.app.Pages;
@@ -16,6 +16,7 @@ import org.nting.toolkit.component.Alignment;
 import org.nting.toolkit.component.Button;
 import org.nting.toolkit.component.Orientation;
 import org.nting.toolkit.component.Panel;
+import org.nting.toolkit.component.RadioButtonGroup;
 import org.nting.toolkit.component.StandardPopup;
 import org.nting.toolkit.component.TooltipPopup;
 import org.nting.toolkit.component.WindowPopup;
@@ -41,23 +42,27 @@ public class PopupTestPage implements IPageFactory {
                 "100dlu, pref, pref, 20dlu, pref, pref, 0px:grow");
         Panel panel = new Panel(layout);
 
-        Property<PopupType> popupTypeProperty = new ObjectProperty<>(PopupType.TOOLTIP);
-        ActionListener popupActionHandler = new PopupActionHandler(popupTypeProperty);
+        RadioButtonGroup radioButtonGroup = new RadioButtonGroup();
+        ActionListener popupActionHandler = new PopupActionHandler(
+                radioButtonGroup.value.transform(type -> (PopupType) type));
 
         panel.addComponent(newButton("0", popupActionHandler), xy(1, 2));
         panel.addComponent(newButton("1", popupActionHandler), xy(1, 1));
-        panel.addComponent(newButton("2", popupActionHandler), xy(3, 1));
-        panel.addComponent(newButton("3", popupActionHandler), xy(3, 2));
+        panel.addComponent(newButton("2", popupActionHandler), xy(3, 2));
+        panel.addComponent(newButton("3", popupActionHandler), xy(3, 1));
         panel.addComponent(newButton("4", popupActionHandler), xy(3, 4));
         panel.addComponent(newButton("5", popupActionHandler), xy(3, 5));
-        panel.addComponent(newButton("6", popupActionHandler), xy(1, 5));
-        panel.addComponent(newButton("7", popupActionHandler), xy(1, 4));
+        panel.addComponent(newButton("6", popupActionHandler), xy(1, 4));
+        panel.addComponent(newButton("7", popupActionHandler), xy(1, 5));
 
         ContainerBuilder<Panel, ?> popupTypeBuilder = panelBuilder("7dlu, pref, 7dlu, pref, 7dlu, pref, 7dlu",
                 "7dlu, pref, 4dlu, pref, 7dlu");
         popupTypeBuilder.addLabel(xyw(1, 1, 5)).text("Select popup type").pass().font(LARGE_FONT, BOLD)
                 .process(l -> l.tooltipText.setValue("Label tooltip test..."));
-
+        popupTypeBuilder.addRadioButton(xy(1, 3), radioButtonGroup).value(PopupType.STANDARD).text("StandardPopup");
+        popupTypeBuilder.addRadioButton(xy(3, 3), radioButtonGroup).value(PopupType.TOOLTIP).text("TooltipPopup")
+                .selected(true);
+        popupTypeBuilder.addRadioButton(xy(5, 3), radioButtonGroup).value(PopupType.WINDOW).text("WindowPopup");
         panel.addComponent(popupTypeBuilder.build(), xyw(0, 0, 5));
         return panel;
     }
@@ -96,10 +101,12 @@ public class PopupTestPage implements IPageFactory {
         }
 
         private TooltipPopup createTooltipPopup(int preferredType) {
-            ContainerBuilder<TooltipPopup, ?> tooltipPopupBuilder = new ContainerBuilder<>(
-                    new TooltipPopup(Alignment.values()[preferredType / 2], Orientation.values()[preferredType % 2],
-                            new FormLayout("2dlu, 80dlu, 2dlu", "2dlu, min(pref;80dlu), 2dlu")));
-            return tooltipPopupBuilder.addMultiLineLabel(xy(1, 1)).text("It's a TooltipPopup").pass()
+            Alignment alignment = Alignment.values()[preferredType / 2];
+            Orientation orientation = Orientation.values()[preferredType % 2];
+            ContainerBuilder<TooltipPopup, ?> tooltipPopupBuilder = new ContainerBuilder<>(new TooltipPopup(alignment,
+                    orientation, new FormLayout("2dlu, 80dlu, 2dlu", "2dlu, min(pref;80dlu), 2dlu")));
+            String text = format("It's a TooltipPopup\n- {}\n- {}" + alignment, orientation);
+            return tooltipPopupBuilder.addMultiLineLabel(xy(1, 1)).text(text).pass()
                     .set("color", tooltipPopupBuilder.build().color).end().build();
         }
 
